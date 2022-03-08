57583ccd367ab3976ad530f78be494fa84da435c
Petr Kracík <petrkr@petrkr.net>
2022-03-08 15:49:13 +0100

    Reordered/refactored code

--- attiny-i2c-slave.ino
+++ attiny-i2c-slave.ino
@@ -20,11 +20,12 @@ void setup() {
   strip.show();
 }
 
-void set_num_leds() {
-  byte len = Wire.read();
-  strip.updateLength(len);
-}
+// ************************
+// ** I2C Slave routines **
+// ************************
 
+byte i2c_register;
+byte i2c_systemreg;
 
 void set_led(int howMany) {
   for (int i = 0; i < howMany / 4; i++) {
@@ -39,37 +40,45 @@ void set_led(int howMany) {
   strip.show();
 }
 
-void fill_led() {
-  byte from = Wire.read();
-  byte count = Wire.read();
-  byte r = Wire.read();
-  byte g = Wire.read();
-  byte b = Wire.read();
-  uint32_t color = strip.Color(r, g, b);
+// I2C Write
+void receiveEvent(int howMany) {
+  i2c_register = Wire.read();
+  i2c_systemreg = Wire.read();
 
-  strip.fill(color, from, count);
-  strip.show();
+  switch (i2c_register) {
+    case 0x01:
+      handle_system(howMany - 2);
+      break;
+
+    case 0x05:
+      handle_strip(howMany - 2);
+      break;
+  }
+
+  // Flush extra buffer
+  while (Wire.available()) {
+    char c = Wire.read();
+  }
 }
 
-void set_brightness() {
-  byte bright = Wire.read();
+// *********************************
+// ** Handle system routines 0x01 **
+// *********************************
 
-  strip.setBrightness(bright);
-  strip.show();
+void handle_system(int howMany) {
 }
 
-void set_rainbow() {
-  byte hi = Wire.read();
-  byte lo = Wire.read();
-
-  uint16_t hue =  hi << 8 | lo;
-
-  strip.rainbow(hue);
-  strip.show();
-}
+// **************************
+// ** WS Led routines 0x05 **
+// **************************
 
 void handle_strip(int howMany) {
-  switch (Wire.read()) {
+  // we have read request
+  if (howMany == 0) {
+    return;
+  }
+
+  switch (i2c_systemreg) {
     case 0x03:
       set_num_leds();
       break;
@@ -79,7 +88,7 @@ void handle_strip(int howMany) {
       break;
 
     case 0x05:
-      set_led(howMany - 1);
+      set_led(howMany);
       break;
 
     case 0x06:
@@ -92,22 +101,56 @@ void handle_strip(int howMany) {
   }
 }
 
-void receiveEvent(int howMany) {
-  if (howMany == 1) {
-    byte reg = Wire.read();
-    return;
-  }
+// 0x03
+void set_num_leds() {
+  byte len = Wire.read();
+  strip.updateLength(len);
+}
 
-  switch (Wire.read()) {
-    case 0x05:
-      handle_strip(howMany - 1);
-      break;
-  }
+// 0x04
+void set_brightness() {
+  byte bright = Wire.read();
 
-  // Flush extra buffer
-  while (Wire.available()) {
-    char c = Wire.read();
+  strip.setBrightness(bright);
+  strip.show();
+}
+
+// 0x05
+void set_led(int howMany) {
+  for (int i = 0; i < howMany / 4; i++) {
+    int led = Wire.read();
+    byte r = Wire.read();
+    byte g = Wire.read();
+    byte b = Wire.read();
+    uint32_t color = strip.Color(r, g, b);
+
+    strip.setPixelColor(led, color);
   }
+  strip.show();
+}
+
+// 0x06
+void set_rainbow() {
+  byte hi = Wire.read();
+  byte lo = Wire.read();
+
+  uint16_t hue =  hi << 8 | lo;
+
+  strip.rainbow(hue);
+  strip.show();
+}
+
+// 0x08
+void fill_led() {
+  byte from = Wire.read();
+  byte count = Wire.read();
+  byte r = Wire.read();
+  byte g = Wire.read();
+  byte b = Wire.read();
+  uint32_t color = strip.Color(r, g, b);
+
+  strip.fill(color, from, count);
+  strip.show();
 }
 
 void loop() {
